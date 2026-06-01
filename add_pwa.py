3ba1f54af787ef4355527a913b85aa9a65b4c3c1
finessehumxn <finessehumxn@gmail.com>
2026-06-01 00:44:49 -0700

    feat: PWA + Android setup

--- add_pwa.py
+++ add_pwa.py
@@ -1,27 +1,14 @@
 ﻿import os
-
 FILE = os.path.join('frontend', 'index.html')
 with open(FILE, 'r', encoding='utf-8') as f:
     h = f.read()
-
-# Add PWA meta tags before </head>
-pwa_head = '''<link rel="manifest" href="/manifest.json">
-<meta name="theme-color" content="#0D4855">
-<meta name="apple-mobile-web-app-capable" content="yes">
-<meta name="apple-mobile-web-app-title" content="MedCompanion AI">'''
-
-# Add service worker before </body>
-pwa_sw = '''<script>
-if ("serviceWorker" in navigator) {
-  navigator.serviceWorker.register("/sw.js");
-}
-</script>'''
-
+pwa_head = '<link rel="manifest" href="/manifest.json">\n<meta name="theme-color" content="#0D4855">\n<meta name="apple-mobile-web-app-capable" content="yes">\n<meta name="apple-mobile-web-app-title" content="MedCompanion AI">'
+pwa_sw = '<script>\nif ("serviceWorker" in navigator) {\n  navigator.serviceWorker.register("/sw.js");\n}\n</script>'
 if 'manifest.json' not in h:
     h = h.replace('</head>', pwa_head + '\n</head>')
     h = h.replace('</body>', pwa_sw + '\n</body>')
     with open(FILE, 'w', encoding='utf-8') as f:
         f.write(h)
-    print("PWA tags added")
+    print("PWA added")
 else:
     print("Already present")
